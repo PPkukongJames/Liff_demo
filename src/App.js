9c2ddaf821a33c82bd0712ce9b03050cc0a4fd4e
pitpibul.k <pitpibul.k>
2023-11-07 14:33:17 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect,useState } from 'react';
+import { useLiff } from 'react-liff';
 
-function App() {
+const App = () => {
+
+  const [userId, setUserId] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const { error, isLoggedIn, isReady, liff } = useLiff();
+  const pathVariable = decodeURIComponent(window.location.search)
+  const params = new URLSearchParams(pathVariable)
+  const psid = params.get("PSID") ? params.get("PSID"):"";
+  const platform = params.get("platform") ? params.get("platform"):"Line";
+  const psidDisplay = <p>PSID : {psid}</p>
+  let fromStyle = {}
+  if(platform==="Line"){
+    fromStyle = {color:'#00B900'}
+  }else{
+    fromStyle = {color:'#1877f2'}
+  }
+
+  const onChangeEmail = event => {
+    setEmail(event.target.value);
+ };
+
+ const onChangePhone = event => {
+  setPhone(event.target.value);
+};
+
+const success =(event)=> {
+  event.preventDefault();
+  console.log(platform);
+  console.log(userId);
+  console.log(email);
+  console.log(phone);
+}
+
+  useEffect(() => {
+    if (!isLoggedIn) return;
+
+    (async () => {
+      const profile = await liff.getProfile();
+      setDisplayName(profile.displayName);
+      setUserId(profile.userId);
+      setEmail(liff.getDecodedIDToken().email);
+    })();
+  }, [liff, isLoggedIn]);
+
+  const showDisplayName = () => {
+    if (error) return <p>Something is wrong.</p>;
+    if (!isReady) return <p>Loading...</p>;
+
+    if (!isLoggedIn) {
+      return (
+        liff.login()
+      );
+    }
+    return (
+      <>
+        <div style={fromStyle}>
+          <p>From : {platform}</p>
+          {platform==='facebook' ? psidDisplay:""}
+        </div>
+
+        <p style={{color:'#A9A9A9'}}>user id : {userId}</p>
+        
+        <div>
+          <p>username : {displayName}</p>
+          <form onSubmit={success}>
+            <p>
+              <label>email : <input type="text" name="email" value={email} onChange={onChangeEmail}/></label>
+            </p>
+            <p>
+              <label>Phone number : <input type="text" name="phone" value={phone} onChange={onChangePhone}/></label>
+            </p>
+            <p>
+            <input style={{marginRight:"20px"}} type="submit" value="Submit" />
+
+            <button className="App-button" onClick={liff.logout}>
+              Log Out
+            </button>
+            </p>
+          </form>
+        </div>
+      </>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{showDisplayName()}</header>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { LiffProvider } from 'react-liff';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+const liffId ='2001245899-jrgM28vo'
+const appStyle = {
+  marginLeft :"20px",
+  display: 'inline-block'
+}
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    
+      <LiffProvider liffId={liffId}>
+      <div style={appStyle}>
+        <App />
+      </div>
+      </LiffProvider>
+   
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
